894bd1ee27ff28991ee9f893a48f4d94c4a0684b
vinilitaiff <vinilitaiff@gmail.com>
2019-07-09 23:40:44 -0400

    iniciando projeto reactjs repoApiGitHub com styled comonents, estilos globais e estiliznado a pagina main

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { FaGithubAlt, FaPlus } from 'react-icons/fa';
 
-// import { Container } from './styles';
+import { Container, Form, SubmitButton } from './styles';
 
-export default function Main() {
-  return <h1>Main</h1>;
+export default class Main extends Component {
+  state = {
+    newRepo: '',
+  };
+
+  handleInputChange = e => {
+    this.state({ newRepo: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    console.log(this.state.newRepo);
+  };
+
+  render() {
+    const { newRepo } = this.state;
+
+    return (
+      <Container>
+        <h1>
+          <FaGithubAlt />
+          Repositorios
+        </h1>
+
+        <Form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="Adicionar Respositorio"
+            value={newRepo}
+            onChange={this.handleInputChange}
+          />
+
+          <SubmitButton disable>
+            <FaPlus color="#FFF" size={14} />
+          </SubmitButton>
+        </Form>
+      </Container>
+    );
+  }
 }
